043d433ae6b6f9a400c84d7a9ce07e1a475ebd49
Carlos <cst.pod@gmail.com>
2023-02-17 11:48:04 +0100

    Reto 13

--- src/Retos10to19/Reto13_FactorialRecursivo.java
+++ src/Retos10to19/Reto13_FactorialRecursivo.java
@@ -9,34 +9,22 @@ package Retos10to19;
  *
  * Enunciado: Escribe una función que calcule y retorne el factorial de un número dado de forma recursiva.
  *
- * Información adicional:
- * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
- * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
- * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
- * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
- *
  */
 
 
 public class Reto13_FactorialRecursivo {
 
-    private static boolean esPalindromo(String texto) {
-
-        texto = texto.replaceAll(" ", "");
-        texto = texto.toLowerCase();
-        int lenght = texto.length();
-        int i = 0;
-        while (i < lenght / 2) {
-            if (texto.charAt(i) != texto.charAt(lenght - 1 - i)) return false;
-            i++;
-        }
-
-        return true;
+    private static long factorialRecursivo(long number) {
+        if (number < 1) return 1;
+        else return number * factorialRecursivo(number-1);
     }
 
     public static void main(String[] args) {
-        System.out.println("Es palindromo?: " + esPalindromo("Ana lleva al oso la avellana"));
-        System.out.println("Es palindromo?: " + esPalindromo("An lleva al oso la avellana"));
+        System.out.println("Factorial de 1: " + factorialRecursivo(1));
+        System.out.println("Factorial de 2: " + factorialRecursivo(2));
+        System.out.println("Factorial de 3: " + factorialRecursivo(3));
+        System.out.println("Factorial de 5: " + factorialRecursivo(5));
+        System.out.println("Factorial de 10: " + factorialRecursivo(10));
     }
 
 }
